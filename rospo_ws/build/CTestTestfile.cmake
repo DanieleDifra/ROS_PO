d42f12266100e01b4241671aa76e6305ad0a2d55
DanieleDifra <daniele.df98@gmail.com>
2020-04-27 15:43:07 +0200

    FIX1.2 gitignore

--- rospo_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/ROS_PO/rospo_ws/src
-# Build directory: /home/ros/ROS_PO/rospo_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("pub_sub")
--- rospo_ws/build/pub_sub/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/ROS_PO/rospo_ws/src/pub_sub
-# Build directory: /home/ros/ROS_PO/rospo_ws/build/pub_sub
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
