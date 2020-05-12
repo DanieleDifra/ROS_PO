49399e3d2a64100e886df959a7cd0e28e7309a5b
Luca <bibo.luke@gmail.com>
2020-05-12 23:21:44 +0200

    [NULL]Tentativi inuliti di capire come diavolo è stato scritto l'urdf di questo robot

---
+++ src/kinsim_2link_planar/ert_main.cpp
@@ -0,0 +1,121 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: ert_main.cpp
+//
+// Code generated for Simulink model 'kinsim_2link_planar'.
+//
+// Model version                  : 1.122
+// Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+// C/C++ source code generated on : Tue May 12 22:47:11 2020
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "kinsim_2link_planar.h"
+#include "kinsim_2link_planar_private.h"
+#include "rtwtypes.h"
+#include "limits.h"
+#include "rt_nonfinite.h"
+#include "linuxinitialize.h"
+#define UNUSED(x)                      x = x
+#define NAMELEN                        16
+
+// Function prototype declaration
+void exitFcn(int sig);
+void *terminateTask(void *arg);
+void *baseRateTask(void *arg);
+void *subrateTask(void *arg);
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = true;
+sem_t stopSem;
+sem_t baserateTaskSem;
+pthread_t schedulerThread;
+pthread_t baseRateThread;
+void *threadJoinStatus;
+int terminatingmodel = 0;
+void *baseRateTask(void *arg)
+{
+  runModel = (rtmGetErrorStatus(kinsim_2link_planar_M) == (NULL)) &&
+    !rtmGetStopRequested(kinsim_2link_planar_M);
+  while (runModel) {
+    sem_wait(&baserateTaskSem);
+    kinsim_2link_planar_step();
+
+    // Get model outputs here
+    stopRequested = !((rtmGetErrorStatus(kinsim_2link_planar_M) == (NULL)) &&
+                      !rtmGetStopRequested(kinsim_2link_planar_M));
+    runModel = !stopRequested;
+  }
+
+  runModel = 0;
+  terminateTask(arg);
+  pthread_exit((void *)0);
+  return NULL;
+}
+
+void exitFcn(int sig)
+{
+  UNUSED(sig);
+  rtmSetErrorStatus(kinsim_2link_planar_M, "stopping the model");
+}
+
+void *terminateTask(void *arg)
+{
+  UNUSED(arg);
+  terminatingmodel = 1;
+
+  {
+    runModel = 0;
+  }
+
+  // Disable rt_OneStep() here
+
+  // Terminate model
+  kinsim_2link_planar_terminate();
+  sem_post(&stopSem);
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  UNUSED(argc);
+  UNUSED(argv);
+  void slros_node_init(int argc, char** argv);
+  slros_node_init(argc, argv);
+  rtmSetErrorStatus(kinsim_2link_planar_M, 0);
+
+  // Initialize model
+  kinsim_2link_planar_initialize();
+
+  // Call RTOS Initialization function
+  myRTOSInit(0.05, 0);
+
+  // Wait for stop semaphore
+  sem_wait(&stopSem);
+
+#if (MW_NUMBER_TIMER_DRIVEN_TASKS > 0)
+
+  {
+    int i;
+    for (i=0; i < MW_NUMBER_TIMER_DRIVEN_TASKS; i++) {
+      CHECK_STATUS(sem_destroy(&timerTaskSem[i]), 0, "sem_destroy");
+    }
+  }
+
+#endif
+
+  return 0;
+}
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
