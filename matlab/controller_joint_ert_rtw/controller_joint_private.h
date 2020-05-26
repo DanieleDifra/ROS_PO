89054ecd2f2655057b941072aea62f2dac2762f1
Paolo-Fusco <paolo.fusco@mail.polimi.it>
2020-05-26 17:20:20 +0200

    [FIX] Utilizzo della pianificazione della traiettoria dei giunti come riferimento del controllore

---
+++ matlab/controller_joint_ert_rtw/controller_joint_private.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: controller_joint_private.h
+//
+// Code generated for Simulink model 'controller_joint'.
+//
+// Model version                  : 1.35
+// Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+// C/C++ source code generated on : Tue May 26 16:55:34 2020
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_controller_joint_private_h_
+#define RTW_HEADER_controller_joint_private_h_
+#include "rtwtypes.h"
+#endif                                // RTW_HEADER_controller_joint_private_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
---
+++ matlab/controller_joint_ert_rtw/ert_main.cpp
@@ -0,0 +1,118 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: ert_main.cpp
+//
+// Code generated for Simulink model 'controller_joint'.
+//
+// Model version                  : 1.35
+// Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+// C/C++ source code generated on : Tue May 26 16:55:34 2020
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Generic->Unspecified (assume 32-bit Generic)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "controller_joint.h"
+#include "controller_joint_private.h"
+#include "rtwtypes.h"
+#include "limits.h"
+#include "linuxinitialize.h"
+#define UNUSED(x)                      x = x
+#define NAMELEN                        16
+
+// Function prototype declaration
+void exitFcn(int sig);
+void *terminateTask(void *arg);
+void *baseRateTask(void *arg);
+void *subrateTask(void *arg);
+volatile boolean_T stopRequested = false;
+volatile boolean_T runModel = true;
+sem_t stopSem;
+sem_t baserateTaskSem;
+pthread_t schedulerThread;
+pthread_t baseRateThread;
+void *threadJoinStatus;
+int terminatingmodel = 0;
+void *baseRateTask(void *arg)
+{
+  runModel = (rtmGetErrorStatus(controller_joint_M) == (NULL));
+  while (runModel) {
+    sem_wait(&baserateTaskSem);
+    controller_joint_step();
+
+    // Get model outputs here
+    stopRequested = !((rtmGetErrorStatus(controller_joint_M) == (NULL)));
+    runModel = !stopRequested;
+  }
+
+  runModel = 0;
+  terminateTask(arg);
+  pthread_exit((void *)0);
+  return NULL;
+}
+
+void exitFcn(int sig)
+{
+  UNUSED(sig);
+  rtmSetErrorStatus(controller_joint_M, "stopping the model");
+}
+
+void *terminateTask(void *arg)
+{
+  UNUSED(arg);
+  terminatingmodel = 1;
+
+  {
+    runModel = 0;
+  }
+
+  // Disable rt_OneStep() here
+
+  // Terminate model
+  controller_joint_terminate();
+  sem_post(&stopSem);
+  return NULL;
+}
+
+int main(int argc, char **argv)
+{
+  UNUSED(argc);
+  UNUSED(argv);
+  void slros_node_init(int argc, char** argv);
+  slros_node_init(argc, argv);
+  rtmSetErrorStatus(controller_joint_M, 0);
+
+  // Initialize model
+  controller_joint_initialize();
+
+  // Call RTOS Initialization function
+  myRTOSInit(0.01, 0);
+
+  // Wait for stop semaphore
+  sem_wait(&stopSem);
+
+#if (MW_NUMBER_TIMER_DRIVEN_TASKS > 0)
+
+  {
+    int i;
+    for (i=0; i < MW_NUMBER_TIMER_DRIVEN_TASKS; i++) {
+      CHECK_STATUS(sem_destroy(&timerTaskSem[i]), 0, "sem_destroy");
+    }
+  }
+
+#endif
+
+  return 0;
+}
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
